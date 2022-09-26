974a8a00abc3df87006de295c25d53fa7f55ffbc
Ivan Yv <steel-97@mail.ru>
2022-09-26 03:29:56 +0300

    exported more types

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import CVeilAddress from './veil/CVeilAddress';
 import CVeilStealthAddress from './veil/CVeilStealthAddress';
 
 import Lightwallet from './veil/Lightwallet';
-import LightwalletAccount from './veil/LightwalletAccount';
+import LightwalletAccount, { AccountType } from './veil/LightwalletAccount';
 import LightwalletAddress from './veil/LightwalletAddress';
 
 import LightwalletTransactionBuilder from './veil/LightwalletTransactionBuilder';
@@ -11,14 +11,17 @@ import CWatchOnlyTx from './veil/tx/CWatchOnlyTx';
 import CWatchOnlyTxWithIndex from './veil/tx/CWatchOnlyTxWithIndex';
 import PublishTransactionResult from './models/PublishTransactionResult';
 import RpcRequester from './veil/RpcRequester';
+import { mainNetParams } from './veil/Chainparams';
 
 // address
 export { CVeilAddress, CVeilStealthAddress };
 // tx builder
 export { LightwalletTransactionBuilder };
 // lightwallet
-export { Lightwallet, LightwalletAccount, LightwalletAddress };
+export { Lightwallet, LightwalletAccount, AccountType, LightwalletAddress };
 export { CWatchOnlyTx, CWatchOnlyTxWithIndex, CLightWalletAnonOutputData };
 // rpc
 export { RpcRequester };
 export type { PublishTransactionResult };
+// misc
+export { mainNetParams }
