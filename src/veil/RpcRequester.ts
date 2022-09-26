4c7476646f0fbc9b1b172e7c52f1aa368d495ce3
Ivan Yv <steel-97@mail.ru>
2022-09-26 04:54:01 +0300

    replaced node-fetch with axios

--- src/veil/RpcRequester.ts
+++ src/veil/RpcRequester.ts
@@ -1,21 +1,10 @@
-import fetch from "node-fetch";
+import axios from "axios";
 import RPCRequest from "../models/rpc/RpcRequest";
 
 export default class RpcRequester {
     public static NODE_URL = "https://explorer-api.veil-project.com";
     public static async send<T>(request: RPCRequest) {
-        const rawResponse = await fetch(RpcRequester.NODE_URL, {
-            method: "POST",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(request, (_, value) =>
-                typeof value === "bigint"
-                    ? parseInt(value.toString()) // because veil node expect integer
-                    : value)
-        });
-
-        return (await rawResponse.json()) as T;
+        const rawResponse = await axios.post<T>(RpcRequester.NODE_URL, request);
+        return rawResponse.data;
     }
 }
