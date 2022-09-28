0408edb66a95e4c3f491c6ff69cdde6dad244222
Ivan Yv <steel-97@mail.ru>
2022-09-28 05:04:25 +0300

    Improved RpcRequester

--- src/veil/RpcRequester.ts
+++ src/veil/RpcRequester.ts
@@ -3,8 +3,13 @@ import RPCRequest from "../models/rpc/RpcRequest";
 
 export default class RpcRequester {
     public static NODE_URL = "https://explorer-api.veil-project.com";
+    public static NODE_PASSWORD = "";
+
     public static async send<T>(request: RPCRequest) {
-        const rawResponse = await axios.post<T>(RpcRequester.NODE_URL, request);
+        const rawResponse = await axios.post<T>(RpcRequester.NODE_URL, request, {
+            headers: { 'Authorization': + `Basic ${RpcRequester.NODE_PASSWORD}` },
+            withCredentials: true
+        });
         return rawResponse.data;
     }
 }
